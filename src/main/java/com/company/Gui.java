e4e234e41f608a1aef91f77f0b22c4e8825eef85
Aya Fahim <bvf-2002@live.dk>
2020-10-27 16:46:33 +0100

    Felterne på brættet er tilføjet

--- src/main/java/com/company/Gui.java
+++ src/main/java/com/company/Gui.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import gui_fields.GUI_Chance;
-import gui_fields.GUI_Field;
-import gui_fields.GUI_Shipping;
-import gui_fields.GUI_Start;
+import gui_fields.*;
 import gui_main.GUI;
 
 import java.awt.*;
@@ -15,17 +12,56 @@ public class Gui {
 
 
     public static void createGUI() {
-        fields = new GUI_Field[3];
+        fields = new GUI_Field[12];
         fields[0] = new GUI_Start();
         fields[0].setTitle("Start");
 
         fields[1] = new GUI_Shipping();
-        fields [1].setBackGroundColor(Color.GRAY);
+        fields[1].setBackGroundColor(Color.GRAY);
         fields[1].setTitle("Tower");
 
         fields[2] = new GUI_Chance();
         fields[2].setBackGroundColor(Color.GREEN);
         fields[2].setTitle("Krater");
+
+        fields[3] = new GUI_Chance();
+        fields[3].setBackGroundColor(Color.YELLOW);
+        fields[3].setTitle("Gold Palace");
+
+        fields[4] = new GUI_Brewery();
+        fields[4].setBackGroundColor(Color.YELLOW);
+        fields[4].setTitle("Gold Palace");
+
+        fields[5] = new GUI_Refuge();
+        fields[5].setBackGroundColor(Color.DARK_GRAY);
+        fields[5].setTitle("Cold Desert");
+
+        fields[6] = new GUI_Street();
+        fields[6].setBackGroundColor(Color.YELLOW);
+        fields[6].setTitle("Walled City");
+
+        fields[7] = new GUI_Chance();
+        fields[7].setBackGroundColor(Color.DARK_GRAY);
+        fields[7].setTitle("Monastery");
+
+        fields[8] = new GUI_Chance();
+        fields[8].setBackGroundColor(Color.PINK);
+        fields[8].setTitle("Black Cave");
+
+        fields[9] = new GUI_Chance();
+        fields[9].setBackGroundColor(Color.WHITE);
+        fields[9].setTitle("Huts in the mountain");
+
+        fields[10] = new GUI_Chance();
+        fields[10].setBackGroundColor(Color.LIGHT_GRAY);
+        fields[10].setTitle("The Pit");
+
+        fields[11] = new GUI_Chance();
+        fields[11].setBackGroundColor(Color.YELLOW);
+        fields[11].setTitle("Goldmine");
+
+
+
 
         gui = new GUI(fields);
     }

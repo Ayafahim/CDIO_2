4f552ab4231275a07c5b25bba50df99b94a7b02b
Sheba Pirani <s205476@student.dtu.dk>
2020-10-27 18:39:07 +0100

    Opdateret main klassen

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -2,10 +2,13 @@ package com.company;
 
 public class Main {
 
-
+    public static Players player1;
+    public static Players player2;
+    public static Game game = new Game();
 
     public static void main(String[] args) {
         Gui.createGUI();
+
 
 
     }

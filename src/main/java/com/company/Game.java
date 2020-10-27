a7b0eabb1a66a03ae0f14d8c3d108a1770ab2644
Mikail <mikail38@live.dk>
2020-10-27 16:30:00 +0100

    Startet på Game klassen.

--- src/main/java/com/company/Game.java
+++ src/main/java/com/company/Game.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class Game {
+
+    // En boolean som holder øje med om spillet stadig er igang
+    private boolean isPlaing = true;
+
+    // En int som holder øje med hvis tur det er. 0 = player1 og 1 = player2
+    private int turn = 0;
+
+    // Opretter to instance af Die klassen, som skal fungere som vores to terninger
+    Die dice1 = new Die(1,6);
+    Die dice2 = new Die(1,6);
+
+    // Field points
+    private final int[] fieldPoints = {250, -100, 100, -20, 180, 0, -70, 60, -80, -50, 650};
+
+
+
 }

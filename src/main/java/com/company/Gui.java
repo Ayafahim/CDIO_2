4a81792b04c6f6f2a882e0d23b8ec8d819a5e2ce
Mikail <mikail38@live.dk>
2020-10-27 16:12:51 +0100

    Tilføjet en GUI til projektet.

--- src/main/java/com/company/Gui.java
+++ src/main/java/com/company/Gui.java
@@ -1,7 +1,33 @@
 package com.company;
 
+import gui_fields.GUI_Chance;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Shipping;
+import gui_fields.GUI_Start;
+import gui_main.GUI;
+
+import java.awt.*;
+
 public class Gui {
 
+    public static GUI gui;
+    public static GUI_Field[] fields;
 
+
+    public static void createGUI() {
+        fields = new GUI_Field[3];
+        fields[0] = new GUI_Start();
+        fields[0].setTitle("Start");
+
+        fields[1] = new GUI_Shipping();
+        fields [1].setBackGroundColor(Color.GRAY);
+        fields[1].setTitle("Tower");
+
+        fields[2] = new GUI_Chance();
+        fields[2].setBackGroundColor(Color.GREEN);
+        fields[2].setTitle("Krater");
+
+        gui = new GUI(fields);
+    }
 
 }

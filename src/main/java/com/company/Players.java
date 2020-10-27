12646e150ddc0adbbedae42e1551bd7ef0e19c31
Jawahir farah <tbs7bjawahir@gmail.com>
2020-10-27 17:22:31 +0100

    Player klasse ændret: Man kan nu tilføje spiller og modtager indput fra brugeren

--- src/main/java/com/company/Players.java
+++ src/main/java/com/company/Players.java
@@ -1,8 +1,19 @@
 package com.company;
-
+import gui_fields.GUI_Player;
+import static com.company.Gui.gui;
 public class Players {
 
+    // String der får bruger input om brugerens navn
+    public String spillerNavn = gui.getUserString("Indtast et navn");
+    public GUI_Player guiPlayer = new GUI_Player(spillerNavn);
+    public int playerPosition =0;
 
+    // metode der tilføjer spilleren
+
+    public void addPlayer(){
+        guiPlayer.getPrimaryColor();
+        gui.addPlayer(guiPlayer);
+    }
 
 
 
